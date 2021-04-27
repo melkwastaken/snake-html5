9bddea46914b7663dd61fb8b876156f70d56293f
melkwastaken <77582036+melkwastaken@users.noreply.github.com>
2021-04-27 18:07:29 +0100

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -11,6 +11,30 @@ var Menu = {
         // Add a sprite to your game, here the sprite will be the game's logo
         // Parameters are : X , Y , image name (see above) 
         this.add.sprite(0, 0, 'menu');
+var Menu = {
+
+    preload : function() {
+        // Load all the needed resources for the menu.
+        game.load.image('menu', './assets/images/menu.png');
+    },
+
+    create: function () {
+
+        // Add menu screen.
+        // It will act as a button to start the game.
+        this.add.button(0, 0, 'menu', this.startGame, this);
+
+    },
+
+    startGame: function () {
+
+        // Change the state to the actual game.
+        this.state.start('Game');
+
+    }
+
+};
+
     }
 
 };
